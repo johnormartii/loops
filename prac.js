5e5e5332de7e229c294ec524694e919a0427aed1
john ormartii <johnormartii@gmail.com>
2023-10-03 20:18:10 +0300

    java script practice

---
+++ prac.js
@@ -0,0 +1,28 @@
+//if else statement
+let number = 55;
+if(number>45){
+   console.log("student is a genius");
+}else{
+   console.log("student failed seriously");
+}
+
+// while loops
+var x = 1;
+while(x<=4){
+    console.log("the value of x:", x);
+
+    x++;
+}
+
+var x = 3;
+while(x<=4){
+    console.log("the value of x:", x);
+
+    x++;
+}
+var x = 2;
+while(x<=4){
+    console.log("the value of x:", x);
+
+    x++;
+}
